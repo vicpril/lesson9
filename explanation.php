7457d46702d58ea6d829a12e4ad8725257a18ffe
Viktor Prilepin <vicpride@gmail.com>
2015-04-05 20:24:43 +0600

    first commit

--- explanation.php
+++
@@ -1,1 +0,0 @@
-a:1:{i:0;a:11:{s:7:"private";s:1:"0";s:11:"seller_name";s:6:"victor";s:5:"email";s:12:"my@email.com";s:11:"allow_mails";s:7:"checked";s:5:"phone";s:11:"89529007170";s:11:"location_id";s:6:"641780";s:11:"category_id";s:3:"112";s:5:"title";s:9:"freelance";s:11:"description";s:19:"I'm good freelancer";s:5:"price";d:50000;s:10:"button_add";s:33:"Подать объявление";}}
